8954c924b3fad26ec9b5e9c384c0d5cdcbbef657
tanakanaoko <nakochi.xxx@gmail.com>
2020-01-08 22:01:50 +0900

    Initial Commit

---
+++ RunningGirl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunningGirl
+//
+//  Created by 田中奈穂子 on 2020/01/08.
+//  Copyright © 2020 tanaka naoko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

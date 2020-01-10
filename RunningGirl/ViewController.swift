84b84ffefb3d9af125ba51d773787f8e36abff3f
tanakanaoko <nakochi.xxx@gmail.com>
2020-01-10 22:38:21 +0900

    Running Girl

--- RunningGirl/ViewController.swift
+++ RunningGirl/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var runnerImageView: UIImageView!
+    
+    @IBOutlet weak var runBottun: UIButton!
+    
+    
+    var imageArray = [UIImage]()
+    
+    var timer1 = Timer()
+    
+    var countUp = 0
+    
+    var timer2 = Timer()
+    
+    var updateCount = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let image0 = UIImage(named: "dash0.png")
+        let image1 = UIImage(named: "dash1.png")
+        let image2 = UIImage(named: "dash2.png")
+        let image3 = UIImage(named: "dash3.png")
+        let image4 = UIImage(named: "dash4.png")
+        let image5 = UIImage(named: "dash5.png")
+        
+        imageArray = [image0!,image1!,image2!,image3!,image4!,image5!]
     }
 
-
+    
+    @IBAction func run(_ sender: Any) {
+        
+        runBottun.isEnabled = false
+        
+        timer1 = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
+            self.countUp = self.countUp + 1
+            self.countLabel.text = String(self.countUp)
+        })
+        
+        timer2 = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (timer) in
+            
+            if(self.updateCount > 5){
+                self.updateCount = 0
+                self.runnerImageView.image = UIImage(named: "dash0.png")
+            }else{
+                self.runnerImageView.image =
+                    self.imageArray[self.updateCount]
+            }
+            self.updateCount = self.updateCount + 1
+        })
+    }
+    
+    
+    @IBAction func stop(_ sender: Any) {
+        
+        runBottun.isEnabled = true
+        
+        timer1.invalidate()
+        timer2.invalidate()
+        
+    }
+    
 }
 
